2b7f91c6272b15acb939de6d08ea769fca96e17a
Hanus Valenta <hanus.valenta@gmail.com>
2026-01-19 08:53:35 +0100

    stuff

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float OpenAngle = 90f;
+    public float OpenSpeed = 2f;
+    public bool IsOpen = false;
+
+    public int DoorID;
+
+    private Quaternion _closedRotation;
+    private Quaternion _openRotation;
+
+    private void Start()
+    {
+        _closedRotation = transform.localRotation;
+        _openRotation = _closedRotation * Quaternion.Euler(0, OpenAngle, 0);
+    }
+
+    public void ToggleDoor()
+    {
+        IsOpen = !IsOpen;
+    }
+
+    private void Update()
+    {
+        if (IsOpen)
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, _openRotation, Time.deltaTime * OpenSpeed);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, _closedRotation, Time.deltaTime * OpenSpeed);
+        }
+    }
+}
